7dbe0fc1e2423a97385208f5d01306f0954fcb07
sumeikom <sumeikom@gmail.com>
2021-03-08 21:16:36 -0500

    commit tree 199a0da7653c48334ba497375bf57e459621fa4e

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,36 @@
 def starts_with_a_vowel?(word)
+    if word.scan(/^[aeiouAEIOU]/).empty?
+        # when using .empty Can be used on collections such as Array, Hash, Set etc. Returns true when they have no elements.
+      false
+    else
+      true
+    end
+  end
+  
 
-end
+  def words_starting_with_un_and_ending_with_ing(text)
+    words = text.split(" ")
+    words.grep(/^un\w+ing$/)
+    # why do I have to put a $
+  end  
 
-def words_starting_with_un_and_ending_with_ing(text)
+  def words_five_letters_long(text)
+    words = text.split(" ")
+    words.grep(/\b\w{5}\b/)
+  end
+  
 
-end
+  def first_word_capitalized_and_ends_with_punctuation?(text)
+    if text.scan(/^[A-Z][\s\S]+\.$/).empty?
+        # $ means end of line
+      false
+    else
+      true
+    end
+  end
+  
 
-def words_five_letters_long(text)
-
-end
-
-def first_word_capitalized_and_ends_with_punctuation?(text)
-
-end
-
-def valid_phone_number?(phone)
-
-end
+  def valid_phone_number?(phone)
+    phone.scan(/\b\(?\d{3}\)?\s?-?\d{3}-?\s?\d{4}\b/).empty? ? false : true
+  end
+  
